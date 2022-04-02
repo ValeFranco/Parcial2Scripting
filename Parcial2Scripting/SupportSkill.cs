3f945455ec0b7ffdef32dc38f878678da0c010f0
juanestebanct <78618669+juanestebanct@users.noreply.github.com>
2022-04-02 16:57:24 -0500

    carta2

--- Parcial2Scripting/SupportSkill.cs
+++ Parcial2Scripting/SupportSkill.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace Parcial2Scripting
 {
-    class SupportSkill
+    public class SupportSkill :Carta 
     {
+        internal enum l_effectType {ReduceAP,ReduceRP,ReduceAll,DestroyEquip,RestoreRP}
+        internal l_effectType effectType;
+        private uint eddectPoints;
+
+        public SupportSkill(string name, uint costPoint, l_Rarity rarity, l_effectType effectType, uint eddectPoints): base(name, costPoint,rarity) 
+        {
+            this.effectType = effectType;
+            EddectPoints = eddectPoints;
+        }
+
+        internal uint EddectPoints { get => eddectPoints; set
+            {
+                if (value > 0) eddectPoints = value;
+                else throw new Exception("no puede ser el CostPoint 0 o inferior   ");
+            }
+
+
+
+        }
     }
 }
---
+++ Parcial2Scripting/SupportSkillBase.cs
@@ -0,0 +1,19 @@
+﻿namespace Parcial2Scripting
+{
+    public class SupportSkillBase
+    {
+        protected l_effectType effectType;
+
+        internal uint EddectPoints
+        {
+            get => eddectPoints; set
+            {
+                if (value > 0) eddectPoints = value;
+                else throw new Exception("no puede ser el CostPoint 0 o inferior   ");
+            }
+
+
+
+        }
+    }
+}
