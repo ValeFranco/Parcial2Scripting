f01d973880e5263ba24fb6e7239beab266d207cc
Valentina <valentina.francoa@upb.edu.co>
2022-04-02 17:04:57 -0500

    Character

--- Parcial2Scripting/Character.cs
+++ Parcial2Scripting/Character.cs
@@ -6,7 +6,42 @@ using System.Threading.Tasks;
 
 namespace Parcial2Scripting
 {
-    class Character
+    class Character : Carta
     {
+        private uint attackPoints;
+        private uint resistPoints;
+        internal List<Carta> equip;
+        internal enum l_Afinity
+        {
+            Knight,
+            Mage,
+            Undead
+        };
+        internal l_Afinity afinity;
+
+        public Character(uint attackPoints, uint resistPoints, List<Carta> equip, l_Afinity afinity) : base(name, costPoint, rarity)
+        {
+            this.attackPoints = attackPoints;
+            this.resistPoints = resistPoints;
+            this.equip = equip;
+            this.afinity = afinity;
+        }
+
+        internal uint AttackPoints
+        {
+            get => attackPoints; set
+            {
+                if (value > 0) attackPoints = value;
+                else throw new Exception("Los attackPoints no pueden ser menor a cero");
+            }
+        }
+        internal uint ResistPoints
+        {
+            get => resistPoints; set
+            {
+                if (value > 0) resistPoints = value;
+                else throw new Exception("Los resistPoints no pueden ser menor a cero");
+            }
+        }
     }
 }
