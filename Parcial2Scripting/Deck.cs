e6c3c6900f727bddac4007f4fdc1fcdd4801c532
headjuancho <juan.arciniegasm@upb.edu.co>
2022-04-02 16:56:58 -0500

    clase deck  update

--- Parcial2Scripting/Deck.cs
+++ Parcial2Scripting/Deck.cs
@@ -6,7 +6,48 @@ using System.Threading.Tasks;
 
 namespace Parcial2Scripting
 {
-    class Deck
+    public class Deck
     {
+        private string nombreJugador;
+        internal uint totalPoints;
+        internal List<Carta> cartas;
+        private byte characterCount;
+        private byte equipCount;
+        private byte supportCount;
+
+
+        public Deck(string nombreJugador)
+        {
+            NombreJugador = nombreJugador;
+
+            characterCount = 0;
+            supportCount = 0;
+            equipCount = 0;
+
+            cartas = new List<Carta>();
+
+        }
+
+
+        internal string NombreJugador
+        {
+            get => nombreJugador;
+            set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value) || value.Length < 5)
+                {
+                  throw new Exception("el nombre no puede tener espacios en blanco  ");
+                }
+                else
+                {
+                  nombreJugador = value;
+                }
+
+            }
+
+
+        }
+
+       
     }
 }
