8cc7ace8a2ebd3e0f963fd086111241b8ed46bb7
juanestebanct <78618669+juanestebanct@users.noreply.github.com>
2022-04-02 16:34:25 -0500

    carta
    cosas de carta

--- Parcial2Scripting/Carta.cs
+++ Parcial2Scripting/Carta.cs
@@ -6,7 +6,46 @@ using System.Threading.Tasks;
 
 namespace Parcial2Scripting
 {
-    class Carta
+    
+    abstract class Carta
     {
+        private string name;
+        private uint costPoint;
+        protected enum l_Rarity { Common, Rare, SuperRare, UltraRare };
+        protected l_Rarity rarity;
+
+        protected Carta(string name, uint costPoint, l_Rarity rarity)
+        {
+            this.rarity = rarity;
+            Name = name;
+            CostPoint = costPoint;
+        }
+
+        protected string Name
+        {
+            get => name;
+
+            set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value) || value.Length < 5)
+                {
+                    throw new Exception("el nombre no puede tener espacios en blanco  ");
+                }
+                else
+                {
+                    Name = value;
+                }
+
+            }
+        }
+        internal uint CostPoint { get => costPoint; set
+            {
+                if (value > 0) costPoint = value;
+                else throw new Exception("no puede ser el CostPoint 0 o inferior   ");
+            }
+                
+                
+                
+         }
     }
 }
