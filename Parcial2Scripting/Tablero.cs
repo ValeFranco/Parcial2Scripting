cc6424f49d07f30b174387239076105985e3006b
Valentina <valentina.francoa@upb.edu.co>
2022-04-02 17:31:41 -0500

    Class tablero

--- Parcial2Scripting/Tablero.cs
+++ Parcial2Scripting/Tablero.cs
@@ -8,5 +8,12 @@ namespace Parcial2Scripting
 {
     class Tablero
     {
+        private Deck player1;
+        private Deck player2;
+
+        public void Atacar(Carta cartaP1, Carta cartaP2)
+        {
+
+        }
     }
 }
